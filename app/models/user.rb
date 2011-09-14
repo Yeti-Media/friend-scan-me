5e85d058a68e4b0988e8e02ed6daaa747bfe2b11
Pat270 <pyeo717@gmail.com>
2011-09-14 16:10:09 -0700

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User
   private
 
   def self.add_facebook(user , auth)
-    info = FacebookInfo.new(uid: auth["user_info"]["name"])
+    info = FacebookInfo.new(uid: auth["uid"], user_name: auth["user_info"]["name"])
     user.facebook_info = info
   end
 
@@ -90,4 +90,3 @@ class User
 
 
 end
-
