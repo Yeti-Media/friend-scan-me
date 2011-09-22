b2deac336ddb2efca7b6578dca667846c00d951b
Nicolas Santa <nicolas55ar@gmail.com>
2011-09-22 18:48:02 -0300

    just facebook for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Settings.omniauth.facebook.app_id , Settings.omniauth.facebook.app_secret
+if !Rails.env.production?
   provider :twitter, Settings.omniauth.twitter.consumer_key, Settings.omniauth.twitter.consumer_secret
   provider :linked_in, Settings.omniauth.linkedin.api_key, Settings.omniauth.linkedin.api_secret
   provider :last_fm, Settings.omniauth.last_fm.api_key, Settings.omniauth.last_fm.api_secret
@@ -8,5 +9,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :instagram, Settings.omniauth.instagram.client_id, Settings.omniauth.instagram.client_secret
   provider :tumblr, Settings.omniauth.tumblr.consumer_key, Settings.omniauth.tumblr.consumer_secret
   provider :flickr, Settings.omniauth.flickr.consumer_key, Settings.omniauth.flickr.consumer_secret
+end
 end
 
