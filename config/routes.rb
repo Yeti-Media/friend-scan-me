0cd6477fedb783e807d3df687f874c96f3f99ee4
Nick Treadway <ntreadway@msn.com>
2011-09-23 15:10:13 -0700

    fixed www routing error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Friendscan::Application.routes.draw do
 
   resources :cards
 
-  match '/' => 'cards#index', :constraints => {:subdomain => /.+/ }
   match 'auth/:provider/callback' => 'sessions#create'
   match 'auth/failure' => 'sessions#failure'
   match "/sessions/signout" => "sessions#destroy", :as => :signout
