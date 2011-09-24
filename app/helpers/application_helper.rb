11ed80f9f4608cc53c48d0031debf463d4da3488
Pat270 <pyeo717@gmail.com>
2011-09-24 10:01:20 -0700

    added index title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Return title for each page
+  def title
+    base_title = "Friend.Scan.Me"
+    if @title.nil?
+      base_title
+    else
+      "#{@title} - #{base_title}"
+    end
+  end
+  
 end
