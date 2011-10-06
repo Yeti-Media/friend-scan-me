da45a52998c021658e7f0ad96533cce5896bde50
Nick Treadway <ntreadway@msn.com>
2011-10-05 17:33:27 -0700

    fixed home#index template error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,16 @@ class HomeController < ApplicationController
   layout "application"
   
   def index
-    session[:mobile_view] = false
+    respond_to_mobile "index"
   end
+  
+  private
+  
+  def respond_to_mobile(content)
+    respond_to do |format|
+      format.mobile {render content}
+    end
+  end
+  
   
 end
