2ba54fc0bd7ed53cca60b02533b12de33afc82d2
Pat270 <pyeo717@gmail.com>
2011-10-12 10:57:53 -0700

    deleted specs pending to be written

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -1,38 +1,5 @@
 require 'spec_helper'
 
 describe CardsController do
-  render_views
-  
-  before(:each) do
-    login_with_oath
-  end
-  
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    it "should be successful" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'create'" do
-    it "should be successful" do
-      get 'create'
-      response.should be_success
-    end
-  end
-  
-  describe "GET 'edit'" do
-    it "should be successful" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
+    pending "add some examples to (or delete) #{__FILE__}"
 end
