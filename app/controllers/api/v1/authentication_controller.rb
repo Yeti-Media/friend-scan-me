9bb4651d298d12f4f8606f187680938f1c049438
Nick Treadway <ntreadway@msn.com>
2012-09-10 14:33:07 -0700

    iOS api updates.

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,7 +4,9 @@ class Api::V1::AuthenticationController < Api::V1::BaseController
   def create
     user = User.find_or_create_with_omniauth(params[:auth])
     if user.save
-      render json: {user_id: user.id , status: 'ok'}
+      @current_user = User.find(user.id)
+      render json: {user_id: user.id , status: 'ok',
+                    qr_code: "http://api.qrserver.com/v1/create-qr-code/?data=#{card_landing_url(@current_user.slug)}&size=183x183&background_color=ffffff&color=244468&second_color=39c&prettify=true&" }
     else
       render json: {status: 'error', error: 'The user could not be saved. Try Again'}
     end
@@ -14,6 +16,5 @@ class Api::V1::AuthenticationController < Api::V1::BaseController
     current_user.associate_provider(params[:auth])
     render json: {status: 'ok'}
   end
-
 
 end
--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BaseController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
   before_filter :detect_mobile_app!
 
   protected
