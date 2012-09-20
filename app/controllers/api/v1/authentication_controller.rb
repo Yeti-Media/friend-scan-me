bd531f9ddb70864212065394b68c7caaf22b527e
Nick Treadway <ntreadway@msn.com>
2012-09-19 17:19:25 -0700

    render api user id to string.

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthenticationController < Api::V1::BaseController
     user = User.find_or_create_with_omniauth(params[:auth])
     if user.save
       @current_user = User.find(user.id)
-      render json: {user_id: user.id , status: 'ok',
+      render json: {user_id: user.id.to_s, status: 'ok',
                     qr_code: "http://api.qrserver.com/v1/create-qr-code/?data=#{card_landing_url(@current_user.slug)}&size=183x183&background_color=ffffff&color=244468&second_color=39c&prettify=true&" }
     else
       render json: {status: 'error', error: 'The user could not be saved. Try Again'}
